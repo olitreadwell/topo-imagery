9c047288f5cf4bece2e454b7cf68147420a7cef9
Alice Fage <afage@linz.govt.nz>
2026-07-28 16:15:25 +1200

    refactor: create topo-imagery-stac package TDE-1960 (#1572)
    ### Motivation
    
    We want to update the topo-imagery repo so that we can more easily
    re-use code modules in separate containers. This is the third of several
    changes to move code into separate uv packages so that the codebase is
    easier to work on.
    
    ### Modifications
    
    Moved stac-related functions into a separate `topo-imagery-stac` uv
    package. (Note that the name of the package and the repo will be renamed
    later once all structural refactoring has been done).
    
    ### Verification
    
    Unit tests, end-to-end tests, container build and test standardising
    workflow run.

--- packages/topo-imagery-stac/src/topo_imagery_stac/testing/generators.py
+++ packages/topo-imagery-stac/src/topo_imagery_stac/testing/generators.py
@@ -3,8 +3,7 @@ from os import urandom
 from typing import Callable
 
 from topo_imagery_common.files.checksum import multihash_as_hex
-
-from scripts.stac.imagery.item import STACAsset, STACProcessing, STACProcessingSoftware
+from topo_imagery_stac.imagery.item import STACAsset, STACProcessing, STACProcessingSoftware
 
 
 def fixed_now_function(now: datetime) -> Callable[[], datetime]:
